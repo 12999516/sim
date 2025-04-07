4a08b45be2319c11471e10865661fee26f805fc2
unknown <12999516@itisrossi.vi.it>
2025-04-07 18:21:23 +0200

    finito

---
+++ es/es/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace es
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ctel tele = new Ctel("1234567890", 5);
+            Csim sim = new Csim("0987654321", 10);
+            sim.addCtel(tele);
+            sim.addCtel(new Ctel("1234567890", 10));
+            sim.addCtel(new Ctel("1284564213", 15));
+            Console.WriteLine(sim.totale_minuti());
+        }
+    }
+}
